24297acd32759841cade0b6261bd5fb8e0302af4
yesh thakur <tyesh3651@gmail.com>
2026-02-11 02:11:32 +0530

    first commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Navbar.css";
+import { assets } from "../../assets/assets";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <img src={assets.logo} alt="logo" className="logo" />
+
+      <ul className="navbar-menu">
+        <li>home</li>
+        <li>menu</li>
+        <li>mobile-app</li>
+        <li>contact us</li>
+      </ul>
+
+      <div className="navbar-right">
+        <img src={assets.search_icon} alt="search" />
+
+        <div className="navbar-search-icon">
+          <img src={assets.basket_icon} alt="basket" />
+          <div className="dot"></div>
+        </div>
+
+        <button>sign in</button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
