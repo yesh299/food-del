3dca9b66642e339290e560ed18ff9065b8def558
yesh thakur <tyesh3651@gmail.com>
2026-02-12 00:53:33 +0530

    second commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import { assets } from "../../assets/assets";
 
 const Navbar = () => {
+  const [menu, setMenu] = useState("home");
+
   return (
     <div className="navbar">
       <img src={assets.logo} alt="logo" className="logo" />
 
       <ul className="navbar-menu">
-        <li>home</li>
-        <li>menu</li>
-        <li>mobile-app</li>
-        <li>contact us</li>
+        <li
+          onClick={() => setMenu("home")}
+          className={menu === "home" ? "active" : ""}
+        >
+          home
+        </li>
+
+        <li
+          onClick={() => setMenu("menu")}
+          className={menu === "menu" ? "active" : ""}
+        >
+          menu
+        </li>
+
+        <li
+          onClick={() => setMenu("mobile-app")}
+          className={menu === "mobile-app" ? "active" : ""}
+        >
+          mobile-app
+        </li>
+
+        <li
+          onClick={() => setMenu("contact-us")}
+          className={menu === "contact-us" ? "active" : ""}
+        >
+          contact us
+        </li>
       </ul>
 
       <div className="navbar-right">
@@ -21,7 +46,6 @@ const Navbar = () => {
           <img src={assets.basket_icon} alt="basket" />
           <div className="dot"></div>
         </div>
-
         <button>sign in</button>
       </div>
     </div>
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import "./Home.css";
+import Header from "../../components/Navbar/Header/Header";
+import ExploreMenu from "../../components/ExploreMenu/exploremenu";
+
+const Home = () => {
+  const [category, setCategory] = useState("All");
+
+  return (
+    <div>
+      <Header />
+      <ExploreMenu category={category} setCategory={setCategory} />
+    </div>
+  );
+};
+
+export default Home;
