8ecce1b52fc18b19719bbaebdda4a70f3da3083e
yesh thakur <tyesh3651@gmail.com>
2026-02-25 22:23:56 +0530

    seventh commit

--- src/pages/PlaceOrder/PlaceOrder.jsx
+++ src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./PlaceOrder.css";
+import { StoreContext } from "../../context/StoreContext";
 
 const PlaceOrder = () => {
+  const { getTotalCartAmount } = useContext(StoreContext);
+
   return (
     <form className="place-order">
       <div className="place-order-left">
@@ -28,7 +31,31 @@ const PlaceOrder = () => {
         <input type="text" placeholder="Phone" />
       </div>
 
-      <div className="place-order-right"></div>
+      <div className="place-order-right">
+        <div className="cart-total">
+          <h2>Cart Total</h2>
+          <div>
+            <div className="cart-total-details">
+              <p>Subtotal</p>
+              <p>${getTotalCartAmount()}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <p>Delivery Fee</p>
+              <p>${getTotalCartAmount() === 0 ? 0 : 2}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <b>Total</b>
+              <b>
+                ${getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 2}
+              </b>
+            </div>
+          </div>
+
+          <button>PROCEED TO PAYMENT</button>
+        </div>
+      </div>
     </form>
   );
 };
